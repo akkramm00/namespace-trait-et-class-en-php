32801aac1c41ff6c940d8a433ff0bd5f0da2161c
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-07-12 21:40:46 +0000

    add  all

--- index.php
+++ index.php
@@ -25,6 +25,31 @@ class Magasin{
 	use Adresse;
 }
 
+// les interfaces :
+//Une interface est un bloc de code dans lequel nous pouvons déclarer des méthodes. À la grande différence des classes, dans les interfaces, il est impossible de déclarer des attributs, ou bien d’avoir de la logique
+
+namespace animal;
+class Animal{
+	protected string $nom;
+	
+	public function getNom(){
+		return $this->nom;
+	}
+	
+	public function setNom(string $nom = null){
+		$this->nom = $nom;
+	}
+	public function dort(){
+		echo(‘zzz’);
+	}
+}
+
+interface AnimalInterface{
+public function getNom();
+	public function setNom(string $nom = null);
+	public function dort();
+}
+//Au même titre qu’une classe sert de moule pour un objet, une interface va servir de moule pour une classe. Une classe qui implémente une interface va devoir implémenter la logique de chacune des méthodes définies dans l’interface.
 ?> 
   </body>
 </html>
